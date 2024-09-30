d70ced764634e5a2d51d02d946b98d857fbe2516
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-09-30 12:21:24 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <limits>
+#include <ctype.h> // isdigitを使う
+
+// テンプレートの宣言
+template <typename T>
+// 比較関数（小さい方）
+T Min(T a, T b) { return (a < b) ? a : b; }
+
+// char型のテンプレートの特殊化
+template <> // テンプレートの解除
+char Min<char>(char a, char b)
+{
+	// 文字が数字じゃないとき
+	if (!isdigit(a) || !isdigit(b))
+	{
+		printf("数字以外は代入できません\n");
+		return std::numeric_limits<char>::min(); // エラーの場合のデフォルト値
+	}
+	return (a < b) ? a : b;
+}
 
 int main()
 {
-	printf("Hello, World\n");
-	printf("ハロー、ワールド\n");
-	
+	int a = 32, b = 64;
+	float c = 3.14f, d = 2.78f;
+	double e = 6.39, f = 4.22;
+	char g = 'k', h = '1';
+
+	// Min関数呼び出し
+	printf("int : %d\n", Min(a, b));	 // intの比較
+	printf("float : %f\n", Min(c, d));	 // float型の比較
+	printf("double : %lf\n", Min(e, f)); // double型の比較
+	printf("char : %c\n", Min(g, h));	 // char型の比較
+
 	return 0;
 }
